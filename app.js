2b74bbb2a8874bfb8d002aa010ea87bc95398beb
SESHAPUSIVAKUMAR <123seshapu@gmail.com>
2022-11-22 12:36:06 +0530

    nodejs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const siva=require("http")
-siva.createServer((req,res)=>{
+const http=require("http")
+http.createServer((req,res)=>{
 	res.write("server created")
 	res.write("  \n learning about node.js")
 	res.write("   \n .........")
